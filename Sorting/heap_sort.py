d5fc1fc805afa7badf3f55f1ad2976a93f1d0804
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-05-19 22:26:40 -0700

    Heap Sort Python

--- Sorting/heap_sort.py
+++ Sorting/heap_sort.py
@@ -1,7 +1,9 @@
 from max_heap import MaxHeap
+from heapq import heapify, heappop
 
 
 def heap_sort(arr):
+    """ Heap sort can be done in place using a max heap."""
     max_heap = MaxHeap(arr)
 
     # from [len-1 ... 1]
@@ -12,8 +14,21 @@ def heap_sort(arr):
 
     return max_heap.arr
 
+
 def test_heap_sort():
     sorted_arr = [2, 4, 4, 5, 13, 345, 451, 561]
     arr = [345, 4, 4, 5, 2, 561, 451, 13]
     assert heap_sort(arr) == sorted_arr
 
+
+def heap_sort_py(arr):
+    sorted_arr = []
+    heapify(arr)    # min heap
+    while arr:
+        sorted_arr.append(heappop(arr))
+    return sorted_arr
+
+def test_heap_sort_py():
+    sorted_arr = [2, 4, 4, 5, 13, 345, 451, 561]
+    arr = [345, 4, 4, 5, 2, 561, 451, 13]
+    assert heap_sort_py(arr) == sorted_arr
