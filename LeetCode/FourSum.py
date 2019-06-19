a8509dc024470319e90fb60732f4fe0079d4f0b5
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-06-19 01:53:14 -0700

    4Sum

--- LeetCode/FourSum.py
+++ LeetCode/FourSum.py
@@ -28,3 +28,22 @@ print(four_sum([-3, -2, -1, 0, 0, 1, 2, 3], 0))
 # Why is there a duplicate tuple in the result ? :/
 # result is a SET of tuples. Is there some subtly i'm missing
 # or is this a python bug ?
+
+
+# Neater Solution
+class Solution:
+    def fourSum(self, nums: 'List[int]', target: int) -> 'List[List[int]]':
+        n = len(nums)
+        nums.sort()
+        result = set()
+        for i in range(n):
+            for j in range(i+1, n):
+                seen = set()
+                for k in range(j+1, n):
+                    a, b, d = nums[i], nums[j], nums[k]
+                    c = target - (a + b + d)
+                    if c in seen:
+                        result.add((a, b, c, d))
+                    else:
+                        seen.add(d)
+        return [[num for num in res] for res in result]
