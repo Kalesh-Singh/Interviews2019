3597b38808864a90499558c06a05f1efd9ef158e
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-06-29 14:18:24 -0700

    Iterative Inorder Traversal

---
+++ LeetCode/Binary Trees/iterative_inorder_traversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: 'TreeNode') -> 'List[int]':
+        result = []
+        stack = []
+        curr = root
+
+        while stack or curr is not None:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+
+            curr = stack.pop()
+            result.append(curr.val)
+            curr = curr.right
+
+        return result
