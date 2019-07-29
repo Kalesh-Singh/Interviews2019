1bf484a2bd9d24dd2de62ff327355d861d1d0227
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-07-28 19:52:07 -0700

    Jump Game

---
+++ LeetCode/Greedy Algorithms/jump_game.py
@@ -0,0 +1,24 @@
+class Solution:
+    def jump(self, nums: 'List[int]') -> int:
+        n = len(nums)
+
+        # Try determine whether we can reach the last index
+        # starting from the right.
+        results = [-1] * n
+
+        # We know we can get to the last index from itself
+        # i.e. no jumps
+        results[n - 1] = 0
+
+        for i in range(n - 2, -1, -1):
+            maxJumpIndex = min(i + nums[i], n - 1)
+            for j in range(i + 1, maxJumpIndex + 1):
+                if results[j] >= 0:
+                    # If we can get to the end from j
+                    # and we can get to j from i
+                    # then we can get to the end from i
+                    if results[i] < 0:
+                        results[i] = 1 + results[j]
+                    else:
+                        results[i] = min(1 + results[j], results[i])
+        return results[0]
