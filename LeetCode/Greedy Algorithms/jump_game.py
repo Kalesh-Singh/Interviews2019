a35646af8f2e2d14ced17c50e89c0ce3e8339006
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-07-28 20:08:25 -0700

    Jump Game

--- LeetCode/Greedy Algorithms/jump_game.py
+++ LeetCode/Greedy Algorithms/jump_game.py
@@ -1,24 +1,33 @@
 class Solution:
-    def jump(self, nums: 'List[int]') -> int:
+    def canJump(self, nums: 'List[int]') -> bool:
+        # Solution 1 - Dynamic Programming Bottom Up Approach
+
+        # n = len(nums)
+
+        # # Try determine whether we can reach the last index
+        # # starting from the right.
+        # results = [False] * n
+
+        # # We know we can get to the last index from itself
+        # # i.e. no jumps
+        # results[n - 1] = True
+
+        # for i in range(n - 2, -1, -1):
+        # maxJumpIndex = min(i + nums[i], n - 1)
+        # for j in range(i + 1, maxJumpIndex + 1):
+        #   if results[j]:
+        #       # If we can get to the end from j
+        #       # and we can get to j from i
+        #       # then we can get to the end from i
+        #       results[i] = True
+        #       break
+
+        # return results[0]
+
+        # Solution 2 - Greedy Approach
         n = len(nums)
-
-        # Try determine whether we can reach the last index
-        # starting from the right.
-        results = [-1] * n
-
-        # We know we can get to the last index from itself
-        # i.e. no jumps
-        results[n - 1] = 0
-
-        for i in range(n - 2, -1, -1):
-            maxJumpIndex = min(i + nums[i], n - 1)
-            for j in range(i + 1, maxJumpIndex + 1):
-                if results[j] >= 0:
-                    # If we can get to the end from j
-                    # and we can get to j from i
-                    # then we can get to the end from i
-                    if results[i] < 0:
-                        results[i] = 1 + results[j]
-                    else:
-                        results[i] = min(1 + results[j], results[i])
-        return results[0]
+        last_pos = n - 1
+        for i in range(n - 1, -1, -1):
+            if i + nums[i] >= last_pos:
+                last_pos = i
+        return last_pos == 0
